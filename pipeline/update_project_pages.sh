90b6e4febb432a1bb2e85a129fde5e752da570b5
Steen Kroyer <stek@via.dk>
2023-04-17 14:45:19 +0000

    Ups - paths inside/outside container differs

--- pipeline/update_project_pages.sh
+++ pipeline/update_project_pages.sh
@@ -8,21 +8,18 @@ if [ ! $# -eq 2 ]; then
 fi
 
 REPOSITORY=$(echo "${1}" | tr  '[:upper:]' '[:lower:]' )
-WORKSPACE=$(echo "${2}" | tr  '[:upper:]' '[:lower:]' )
 REPO_OWNER=$(echo "${REPOSITORY}" | cut -f1 -d/ )
 REPO_NAME=$(echo "${REPOSITORY}" | cut -f2 -d/ )
 PAGES_URL="https://${REPO_OWNER}.github.io/${REPO_NAME}/"
 
 echo "Update project pages variables:"
-echo "Current directory = $(pwd)"
 echo "REPOSITORY = ${REPOSITORY}"
-echo "WORKSPACE = ${WORKSPACE}"
 echo "REPO_OWNER = ${REPO_OWNER}"
 echo "REPO_NAME = ${REPO_NAME}"
 echo "PAGES_URL = ${PAGES_URL}"
-ls -al
 
-if [ ! $(grep -q "${PAGES_URL}" /workspaces/iot-base/README.md) ]; then
+
+if [ ! $(grep -q "${PAGES_URL}" README.md) ]; then
     echo "*** Error: The url/link that should point to the GitHub pages for"
     echo "***        the repository should be set to ${PAGES_URL} but is set to"
     echo "***        something else. Fix the link and try again."
