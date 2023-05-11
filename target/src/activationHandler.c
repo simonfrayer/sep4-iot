edc8a0b891e8118b93ae0631e9062e2c955d8ee7
Agata Rudol <315233@via.dk>
2023-05-11 08:55:32 +0000

    changed frequency to 2.5 mins

--- target/src/activationHandler.c
+++ target/src/activationHandler.c
@@ -38,7 +38,7 @@ void activationHandler_task(void* pvParameters)
 	
 	TickType_t xLastWakeTime;
 	xLastWakeTime = xTaskGetTickCount();
-	const TickType_t xFrequency = 15000/portTICK_PERIOD_MS; // 150000 ms = 2.5 mins
+	const TickType_t xFrequency = 150000/portTICK_PERIOD_MS; // 150000 ms = 2.5 mins
 
 	for(;;)
 	{
