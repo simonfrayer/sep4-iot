bceb794ce96998ed8e9c8480df3ceac669cbe7e8
simonfrayer <315183@via.dk>
2023-05-14 11:44:16 +0000

    implemented medianCalculatorTest  no errors shown but cant build because 'undefined reference to `medianCalculator_calculateMedian''

---
+++ test/src/medianCalculator_test.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+#include <stdio.h>
+#include <stdint.h>
+
+extern "C" {
+    #include "../../target/src/include/medianCalculator.h"
+}
+
+class MedianCalculatorTest : public::testing::Test{
+    protected:
+        void SetUp() override{}
+        void TearDown() override{}
+};
+
+// Demonstrate some basic assertions.
+TEST_F(MedianCalculatorTest, CorrectInput) {
+    //Arrange
+    int16_t arr[10] = {100, 50, -45, -400, 500, 257, 234, 353, 608, 15};
+    int arrayLength = sizeof(arr) / sizeof(arr[0]);
+    int16_t result;
+    //Act
+    result = medianCalculator_calculateMedian(arr, arrayLength);
+    //Assert
+    EXPECT_EQ(result, 100);
+}
