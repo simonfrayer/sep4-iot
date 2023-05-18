617e8b8d9acf7c9832bd99c0e9699161b5a4a05f
simonfrayer <315183@via.dk>
2023-05-18 12:01:28 +0000

    ok testing is setup but setting/gettting limits does not work because it is protected by a mutex and we dont know how to initialize it from test folder

--- test/src/dataHandler_test.cpp
+++ test/src/dataHandler_test.cpp
@@ -4,22 +4,39 @@ extern "C" {
     #include <stdlib.h>
     #include <stdio.h>
     #include <stdint.h>
+    #include <semphr.h>
     #include "dataHandler.h"
 }
     
 class DataHandlerTest : public::testing::Test{
     protected:
         void SetUp() override{
+            //create mutex
+            dataHandler_createMutex();
+
             //setting limits
             limit1 = 10;
             limit2 = 30;
             dataHandler_setLimits(limit1, limit2);
         }
-        void TearDown() override{}
+        void TearDown() override{
+            limit1 = 0;
+            limit2 = 0;
+        }
 
         int16_t limit1;
         int16_t limit2;
 };
+
+TEST_F(DataHandlerTest, SetLimits) {
+    //Arrange
+    //Act
+    //Assert
+    EXPECT_NO_THROW({
+        dataHandler_setLimits(limit1, limit2);
+    });
+
+}
 
 TEST_F(DataHandlerTest, GetLimits) {
     //Arrange
